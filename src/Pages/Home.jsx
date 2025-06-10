f2a33aa1479e977171c56c75d8b901807b3fa7cf
Mikaela <ela.jansson@hotmail.com>
2025-06-10 10:06:32 +0200

    updated text homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,8 +3,8 @@ import { Container } from "react-bootstrap";
 function Home() {
   return (
     <Container className="mt-4">
-      <h1>Välkommen till StudentPortalen</h1>
-      <p>Här kan du läsa om kurser, nyheter och registrera dig.</p>
+      <h1>Välkommen till StudentPortalen!</h1>
+      <p>Här kan du läsa om kurser, nyheter och registrera dig. </p>
     </Container>
   );
 }
